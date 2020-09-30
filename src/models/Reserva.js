0d32994510287007b8324cf1421673e1deb0a016
Eduardo Rodríguez <eduardostifler5@gmail.com>
2020-09-30 09:58:11 +0200

    Añadido metodo find id para bookings

--- src/models/Reserva.js
+++ src/models/Reserva.js
@@ -6,7 +6,7 @@ const reservaSchema = new Schema(
         initialDate: {type: Date, required:true},
         finalDate: {type:Date, required:true},
         nSuitcases: {type:Number, required: true},
-        price:{type: Number},
+        price:{type: Number, default: 6},
         client: {type:String, required:true},
         guardian:{type: Schema.Types.ObjectId, ref:"Usuario"},
         review:{type:String}
